c09417af4bc92684e4cd079f434cad2369e13ee3
許皓維 <c9dk.012@gmail.com>
2021-03-15 17:00:26 +0800

    Update items.py

--- project/project/items.py
+++ project/project/items.py
@@ -2,7 +2,6 @@ import scrapy
 
 class ProjectItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
     date = scrapy.Field()
     log = scrapy.Field()
     createdtime = scrapy.Field()
